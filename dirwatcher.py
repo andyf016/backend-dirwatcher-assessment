f68b3ef458f9d9e1ee522707376c69fc1f502924
Andrew Fillenwarth <andyf016@gmail.com>
2020-07-15 23:11:38 -0400

    catching IOErrors

--- dirwatcher.py
+++ dirwatcher.py
@@ -19,6 +19,9 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
+# set logging level
+logger.setLevel(logging.DEBUG)
+
 
 def signal_handler(sig_num, frame):
     """
@@ -34,8 +37,12 @@ def signal_handler(sig_num, frame):
     exit_flag = True
 
 
-def dir_watching():
-    pass
+def dir_watching(fileName):
+    with open(fileName, 'r') as f:
+        text = f.read()
+        print(text)
+    return
+    
 
 
 def create_parser():
@@ -44,11 +51,12 @@ def create_parser():
             that are created within the monitored directory and will
             continually search within all files in the directory for a
             "magic string", which is provided as a command line argument""")
-    parser.add_argument('path', help='directory path to watch')
-    parser.add_argument('magic', help='string to watch for')
+    parser.add_argument('path', nargs='+', type=str, help='directory path to watch')
+    # parser.add_argument('magic', nargs='+', type=str, help='string to watch for')
     parser.add_argument('-e', '--ext', help='text file extension to watch')
     parser.add_argument('-i', '--interval', type=int,
                         help='Number of seconds between polling')
+    return parser
 
 
 def main(args):
@@ -60,23 +68,48 @@ def main(args):
     # Now my signal_handler will get called if OS sends
     # either of these to my process.
 
+    # set start time
+    app_start_time = datetime.datetime.now()
+    path = ''.join(name_space.path)
     if not name_space:
         parser.print_usage()
         sys.exit(1)
 
-    polling_interval = name_space.i
+    if name_space.interval:
+        polling_interval = name_space.interval
+    else:
+        polling_interval = 5
 
+    logger.info(
+        '\n'
+        '---------------------------------------------------------------\n'
+        f'      Running{__file__}\n'
+        f'      Started on {app_start_time.isoformat()}\n'
+        '---------------------------------------------------------------\n'
+        )
     while not exit_flag:
+        print(args)
         try:
-            # call my directory watching function
-            pass
+            dir_watching(path)
+        except IOError:
+            logger.error("Directory or file not found")
+        except FileNotFoundError:
+            logger.error("Directory or file not found")
         except Exception as e:
             # This is an UNHANDLED exception
-            # Log an ERROR level message here
+            logger.error(f" {e} What the hell happened!?")
             pass
 
         # put a sleep inside my while loop so I don't peg the cpu usage at 100%
         time.sleep(polling_interval)
+    uptime = datetime.datetime.now() - app_start_time
+    logger.info(
+        '\n'
+        '-------------------------------------------------------------------\n'
+        f'      Stopped {__file__}\n'
+        f'      Uptime was {str(uptime)}\n'
+        '-------------------------------------------------------------------\n'
+        )
 
     # final exit point happens here
     # Log a message that we are shutting down
